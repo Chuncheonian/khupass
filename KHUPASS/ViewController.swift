55af79161cf7cd93ae2b064258462570d2fb3491
chuncheonian <ehddud2468@gmail.com>
2021-11-17 19:55:40 +0900

    init: init KHUPASS Project

---
+++ KHUPASS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  KHUPASS
+//
+//  Created by dykoon on 2021/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  // MARK: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBrown
+  }
+}
