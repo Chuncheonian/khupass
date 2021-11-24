d1ebdf82334dc953059cd53187cd8c719f91c5c5
chuncheonian <ehddud2468@gmail.com>
2021-11-24 20:47:07 +0900

    feat: add logo image in LaunchScreen
    - LaunchScreen에 로고이미지를 넣었습니다.
    - LaunchScreen를 1초동안 보여주고 다음 화면으로 넘어갑니다.

--- KHUPASS/Support/AppDelegate.swift
+++ KHUPASS/Support/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    sleep(1)
     return true
   }
 
